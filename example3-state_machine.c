f14e94cd617d454d344c9d7c25bbf1185d37cfdc
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-06-15 15:50:37 +0300

    Update example3-state_machine.c

--- example3-state_machine.c
+++ example3-state_machine.c
@@ -1,9 +1,6 @@
 #include "state_machine.h"
 
-extern uint8_t pc_uart_data_rx[37];
 extern volatile bool flg_tx_rx_complete;
-extern uint8_t ppm_spi_data_rx[32];
-extern uint8_t ppm_spi_data_tx[32];
 extern EVENT_t event;
 
 void state_idle() {
@@ -11,8 +8,8 @@ void state_idle() {
 }
 
 void state_rqst_ppm_status() {
-	if( (pc_uart_data_rx[2] | (pc_uart_data_rx[3] << 8) ) == get_crc16_modbus(pc_uart_data_rx, (LEN_MSG_REQUEST_STATUS_PPM_RX - 2)) )
-		send_ppm_status();
+	if( get_crc16_modbus() )
+		send_status();
 	else
 		send_answer(ERROR_CRC);
 	
@@ -26,7 +23,7 @@ void state_echo() {
 }
 
 void state_information_command() {
-	if( (pc_uart_data_rx[35] | (pc_uart_data_rx[36] << 8) ) == get_crc16_modbus(pc_uart_data_rx, (LEN_MSG_INFORMATION_RX - 2)) ) {
+	if( get_crc16_modbus() ) {
 		flg_tx_rx_complete = false;
 		send_answer(ERROR_NONE);
 	} else
@@ -42,10 +39,10 @@ void state_send_answer() {
 }
 
 void state_check_answer_ppm() {
-	if (ppm_check_tx_rx_arrays())
+	if (check_arrays())
 		send_answer(ERROR_NONE);
 	else
-		send_answer(ERROR_PPM_ANSWER);
+		send_answer(ERROR_ANSWER);
 	
 	event = EVENT_NONE;
 }
