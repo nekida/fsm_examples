f2b60374ab0efb8b4d820b3aba0b794284489f51
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-06-15 15:51:33 +0300

    Update example3-state_machine.h

--- example3-state_machine.h
+++ example3-state_machine.h
@@ -4,16 +4,13 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stdio.h"
-#include "ppm.h"
-#include "crc16.h"
-#include "iface_mcu_pc.h"
 #include <string.h>
 
 typedef enum {
 	STATE_IDLE,
-	STATE_RQST_PPM_STATUS,
+	STATE_RQST_STATUS,
 	STATE_INFORMATION_COMMAND,
-	STATE_CHECK_ANSWER_PPM,
+	STATE_CHECK_ANSWER,
 	STATE_ECHO,
 	STATE_SEND_ANSWER,
 	STATE_MAX
@@ -21,7 +18,7 @@ typedef enum {
 
 typedef enum {
 	EVENT_NONE,
-	EVENT_RQST_PPM_STATUS,
+	EVENT_RQST_STATUS,
 	EVENT_INFORMATION_COMMAND,
 	EVENT_ECHO,
 	EVENT_SEND_ANSWER,
@@ -29,10 +26,10 @@ typedef enum {
 } EVENT_t;
 
 void state_idle(void);
-void state_rqst_ppm_status(void);
+void state_rqst_status(void);
 void state_echo(void);
 void state_information_command(void);
 void state_send_answer(void);
-void state_check_answer_ppm(void);
+void state_check_answer(void);
 
 #endif /*__STATE_MACHINE_H */
