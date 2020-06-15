f401770b5d7d145149373f31bdecf1d990e5bf7d
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-06-15 15:47:27 +0300

    Update example2.h

--- example2.h
+++ example2.h
@@ -4,7 +4,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stdio.h"
-#include "ppm.h"
 #include "crc16.h"
 #include <string.h>
 
@@ -12,14 +11,14 @@ typedef void *(*STATE_FUNC_PTR_t)();
 
 typedef enum {
 	EVENT_NONE,
-	EVENT_RQST_PPM_STATUS,
+	EVENT_RQST_STATUS,
 	EVENT_INFORMATION_COMMAND,
 	EVENT_ECHO,
 	EVENT_SEND_ANSWER,
 } EVENT_t;
 
 void *state_idle(void);
-void *state_rqst_ppm_status(void);
+void *state_rqst_status(void);
 void *state_echo(void);
 void *state_information_command(void);
 void *state_send_answer(void);
